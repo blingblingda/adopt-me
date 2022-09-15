4c26008c5b8085b62d9d90e96c530105d4439f16
Belinda Shan <belinda126@live.cn>
2022-09-15 13:45:46 +1000

    added state for location, animal, and breed

--- src/Search/SearchParams.js
+++ src/Search/SearchParams.js
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["1", "2"];
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+            // anytime a change event happens on this particular input, the inside fn will be called, then setLocation will be called, then location in state will be updated to user's input value, then rendered with the new state.
+          />
         </label>
         <label htmlFor="animal">
           Animal
-          <select id="animal">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
           </select>
         </label>
         <label htmlFor="breed">
           Breed
-          <select id="breed">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
           </select>
         </label>
         <button>Submit</button>
