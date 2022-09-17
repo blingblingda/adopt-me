2ee3d9f288320674779a66d97e1f9090f10260ea
Belinda Shan <belinda126@live.cn>
2022-09-17 23:01:49 +1000

    added Modal in Details component

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "../tools/ErrorBoundary";
 import ThemeContext from "../tools/ThemeContext";
+import Modal from "../Modal/Modal";
 
 const WrappedDetails = () => {
   const params = useParams();
@@ -19,7 +20,7 @@ class Details extends Component {
   //   super(props);
   //   this.state = { loading: true };
   // }
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const res = await fetch(
@@ -30,6 +31,8 @@ class Details extends Component {
     this.setState(json.pets[0]);
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
   render() {
     if (this.state.loading) {
       return <h2>Loading...</h2>;
@@ -37,7 +40,7 @@ class Details extends Component {
 
     // throw new Error("lmao you crashed");
 
-    const { name, animal, breed, city, state, description, images } =
+    const { name, animal, breed, city, state, description, images, showModal } =
       this.state;
     return (
       <div className="details">
@@ -47,10 +50,27 @@ class Details extends Component {
           <h2>{`${animal} - ${breed} - ${city},${state}`}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <a href="https://www.rspca.org.au/">Yes</a>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
