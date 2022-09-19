2d44cddd5df73a1b6a62c05508b788028f119b14
Belinda Shan <belinda126@live.cn>
2022-09-19 22:32:42 +1000

    refactor Modal component

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,18 +1,30 @@
-import React, { useRef, useEffect } from "react";
+import React, {
+  FunctionComponent,
+  MutableRefObject,
+  useRef,
+  useEffect,
+} from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
+const Modal: FunctionComponent = ({ children }) => {
   // using Ref to get the exact same thing in every single render
-  const elRef = useRef(null);
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
 
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
   }, []);
 
   //pass the children (whatever put inside <Modal></Modal>) to the portal div.
